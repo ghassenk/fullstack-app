e9c3b3050a40b3e26db27c97b3863bacc5f9aa88
ghassen <ghassenk@gmail.com>
2025-03-21 12:29:09 +0100

    add sample app

---
+++ simple-web-app/app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
+import os
+
+app = Flask(__name__, static_folder='static')
+CORS(app)
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    data = request.json
+    name = data.get('name', 'stranger')
+    print({name})
+    return jsonify({'message': f'Hello, {name}!'})
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
